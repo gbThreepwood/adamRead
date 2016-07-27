760cd0618d37b66840dbccb1cd80a67d80452139
Eirik Haustveit <eirik@la2kta.net>
2016-07-27 23:01:09 +0200

    init.d startup script

--- adam-serial-mqtt.py
+++ adam-serial-mqtt.py
@@ -1,19 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Communication script for ADAM4017 using ADAM4561 USB to RS485 converter
 # Eirik Haustveit, 2016
 
-# In order to communicate with the RS485 bus the RTS line needs to be controlled.
-# When RTS is true the line drivers are enabled, i.e. the converter is in TX mode.
-#
-# In order to receive data the line driver must be disabled by setting the
-# RTS line low(false). The timing of this change is critical, the state must
-# not change to false before the transmission is completed, but it must change before
-# the slave starts transmitting a response.
-#
-#
-# Version 3 of the pySerial library supports a special RS485 mode, where the timing
-# is handled automatically
+# Create a connection to the ADAM4017 module, and read the analog values.
+# Compute the corresponding measurements based upon knowledge of the actual
+# sensors that are connected, i.e. this is a very application spesific
+# script.
 
+import sys
 import serial
 import io
 import time
@@ -24,16 +18,24 @@ import configparser
 
 
 import paho.mqtt.client as mqtt
+
 import logging
+import logging.handlers
 
 from adam4000 import adam4017
+
+#Settings
+CONFIG_FILENAME = '/srv/refrigerators/adamRead/adamread.conf'
+LOG_FILENAME = '/tmp/adamread.log'
+LOG_LEVEL = logging.INFO
 
 #logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 #logger.setLevel(logging.INFO)
 
 # create a file handler
-handler = logging.FileHandler('adamRead.log')
+#handler = logging.FileHandler('adamRead.log')
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
 handler.setLevel(logging.INFO)
 
 # create a logging format
@@ -44,15 +46,32 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
+# Class to capture stdout and sterr in the log
+class serviceLogger(object):
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+
+    def write(self, message):
+        # Check if message is empty
+        if message.rstrip() != "":
+            self.logger.log(self.level, message.rstrip())
+
+# Use logfile as standard output and standard error
+sys.stdout = serviceLogger(logger, logging.INFO)
+sys.stderr = serviceLogger(logger, logging.ERROR)
+
+
 
 def main():
+    print('Starting ADAM4017 temperature and humidity acquisition')
+
+    now = datetime.datetime.now()
+    logger.info('Program executed at: %s ' % now.isoformat())
+
     try:
         config = configparser.ConfigParser()
-        config.read('adamread.conf')
-
-        now = datetime.datetime.now()
-
-        logger.info('Program executed at: %s ' % now.isoformat())
+        config.read(CONFIG_FILENAME)
 
         adam4000conf = config['ADAM4000']
         logger.info('Creating instance of ADAM4017 class.')
@@ -79,10 +98,12 @@ def main():
           
             # Refrigerator A: 
             humidity = (sensor.readAnalogIn(0)-4) * 6.25
+            humidity = float('{0:.1f}'.format(humidity))
             logger.info('Humid A: ' + str(humidity))
            
             temperature = -40 + (sensor.readAnalogIn(1)-4) * 7.5
-    
+            temperature = float('{0:.1f}'.format(temperature))
+            
             logger.info('Temp A: ' + str(temperature))
             
             mqttc.publish("adam4017/temperatureA", temperature)
@@ -99,7 +120,7 @@ def main():
             mqttc.publish("adam4017/temperatureB", temperature)
             mqttc.publish("adam4017/humidityB", humidity)
            
-            time.sleep(1)
+            time.sleep(5)
 
     except KeyError:
         logger.error('Invalid configuration file')
